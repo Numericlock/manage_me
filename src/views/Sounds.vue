4d90d52f83424996b2953544d9751b32a19d44aa
Numericlock <49630768+Numericlock@users.noreply.github.com>
2021-02-02 17:24:32 +0900

    Sounds画面仮デザイン

--- src/views/Sounds.vue
+++ src/views/Sounds.vue
@@ -7,45 +7,49 @@
             </div>
         </div>
         <div class="sounds-wrapper" id="sounds-wrapper">
-            <SoundAndVisualizer ref="SoundAndVisualizer" />
-            <button @click="close">STOP</button>
-            <table>
-                <tr>
-                    <th>Artist</th>
-                    <th>title</th>
-                    <th>album</th>
-                </tr>
-                <tr v-for="(sound, key) in sound_docs" :key="key">
-                    <td>{{ sound.artist }}</td>
-                    <td>{{ sound.name }}</td>
-                    <td>{{ sound.album }}</td>
-                    <td class="td-button">
-                        <svg version="1.1" viewBox="0 0 512 512" xml:space="preserve" @click = "open(sound.path)">
-                            <g>
-                                <path class="st0" d="M256,0C114.625,0,0,114.625,0,256c0,141.374,114.625,256,256,256c141.374,0,256-114.626,256-256
-		C512,114.625,397.374,0,256,0z M351.062,258.898l-144,85.945c-1.031,0.626-2.344,0.657-3.406,0.031
-		c-1.031-0.594-1.687-1.702-1.687-2.937v-85.946v-85.946c0-1.218,0.656-2.343,1.687-2.938c1.062-0.609,2.375-0.578,3.406,0.031
-		l144,85.962c1.031,0.586,1.641,1.718,1.641,2.89C352.703,257.187,352.094,258.297,351.062,258.898z"></path>
-                            </g>
-                        </svg>
-                    </td>
-                    <td class="td-button">
-                        <svg version="1.1" viewBox="0 0 512 512" xml:space="preserve" @click = "modal_open(sound._id)">
-                            <g>
-                                <polygon class="st0" points="367.375,183.607 312.486,238.495 257.592,183.607 240.087,201.105 294.982,256 240.087,310.894 
-		257.592,328.393 312.486,273.498 367.375,328.393 384.88,310.894 329.985,256 384.88,201.105 	"></polygon>
-                                <path class="st0" d="M448.376,60.557h-251.76c-23.23,0-45.327,10.07-60.573,27.608L7.448,236.082C2.5,241.76-0.007,248.92,0,256
-		c-0.007,7.08,2.5,14.234,7.448,19.918l128.596,147.931c15.252,17.532,37.342,27.594,60.573,27.594h251.76
-		c35.144-0.014,63.617-28.48,63.624-63.624V124.187C511.993,89.036,483.52,60.563,448.376,60.557z M484.539,387.819
-		c-0.007,10.03-4.03,18.98-10.593,25.57c-6.591,6.564-15.541,10.579-25.57,10.593h-251.76c-15.286,0-29.82-6.624-39.85-18.155
-		L28.178,257.904c-0.496-0.577-0.711-1.2-0.718-1.904c0.006-0.704,0.221-1.327,0.71-1.891l128.602-147.923
-		c10.03-11.545,24.564-18.168,39.844-18.168h251.76c10.03,0.007,18.98,4.022,25.57,10.593c6.563,6.583,10.586,15.54,10.593,25.576
-		V387.819z"></path>
-                            </g>
-                        </svg>
-                    </td>
-                </tr>
-            </table>
+            <div class="SoundAndVisualizer-wrapper">
+                <SoundAndVisualizer ref="SoundAndVisualizer" />
+            </div>
+            <!--<button @click="close">STOP</button>-->
+            <div class="table-wrapper">
+                <table>
+                    <tr>
+                        <th>Artist</th>
+                        <th>title</th>
+                        <th>album</th>
+                    </tr>
+                    <tr v-for="(sound, key) in sound_docs" :key="key">
+                        <td>{{ sound.artist }}</td>
+                        <td>{{ sound.name }}</td>
+                        <td>{{ sound.album }}</td>
+                        <td class="td-button">
+                            <svg version="1.1" viewBox="0 0 512 512" xml:space="preserve" @click = "open(sound.path)">
+                                <g>
+                                    <path class="st0" d="M256,0C114.625,0,0,114.625,0,256c0,141.374,114.625,256,256,256c141.374,0,256-114.626,256-256
+            C512,114.625,397.374,0,256,0z M351.062,258.898l-144,85.945c-1.031,0.626-2.344,0.657-3.406,0.031
+            c-1.031-0.594-1.687-1.702-1.687-2.937v-85.946v-85.946c0-1.218,0.656-2.343,1.687-2.938c1.062-0.609,2.375-0.578,3.406,0.031
+            l144,85.962c1.031,0.586,1.641,1.718,1.641,2.89C352.703,257.187,352.094,258.297,351.062,258.898z"></path>
+                                </g>
+                            </svg>
+                        </td>
+                        <td class="td-button">
+                            <svg version="1.1" viewBox="0 0 512 512" xml:space="preserve" @click = "modal_open(sound._id)">
+                                <g>
+                                    <polygon class="st0" points="367.375,183.607 312.486,238.495 257.592,183.607 240.087,201.105 294.982,256 240.087,310.894 
+            257.592,328.393 312.486,273.498 367.375,328.393 384.88,310.894 329.985,256 384.88,201.105 	"></polygon>
+                                    <path class="st0" d="M448.376,60.557h-251.76c-23.23,0-45.327,10.07-60.573,27.608L7.448,236.082C2.5,241.76-0.007,248.92,0,256
+            c-0.007,7.08,2.5,14.234,7.448,19.918l128.596,147.931c15.252,17.532,37.342,27.594,60.573,27.594h251.76
+            c35.144-0.014,63.617-28.48,63.624-63.624V124.187C511.993,89.036,483.52,60.563,448.376,60.557z M484.539,387.819
+            c-0.007,10.03-4.03,18.98-10.593,25.57c-6.591,6.564-15.541,10.579-25.57,10.593h-251.76c-15.286,0-29.82-6.624-39.85-18.155
+            L28.178,257.904c-0.496-0.577-0.711-1.2-0.718-1.904c0.006-0.704,0.221-1.327,0.71-1.891l128.602-147.923
+            c10.03-11.545,24.564-18.168,39.844-18.168h251.76c10.03,0.007,18.98,4.022,25.57,10.593c6.563,6.583,10.586,15.54,10.593,25.576
+            V387.819z"></path>
+                                </g>
+                            </svg>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
         <AttentionModal @submit='sound_delete' ref="AttentionModal" v-bind:text="text"/>
     </div>
@@ -263,7 +267,7 @@
     .sounds-setting-wrapper {
         display: flex;
         flex-direction: column;
-        justify-content: space-around;
+        justify-content: space-between;
         height: 100%;
         padding: 20px 0px;
 
@@ -426,8 +430,17 @@
             height: 100%;
             width: 100%;
             overflow-y: auto;
+            overflow-x: hidden;
             color: white;
-
+            .SoundAndVisualizer-wrapper{
+                padding:10px 25px 10px;
+                background: rgba( 62, 62, 62, 0.50 );
+                box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+                backdrop-filter: blur( 5.0px );
+                -webkit-backdrop-filter: blur( 5.0px );
+                border-radius: 10px;
+                border: 1px solid rgba( 255, 255, 255, 0.18 );
+            }
             .sound {
                 display: flex;
                 justify-content: space-between;
@@ -463,23 +476,31 @@
             border-radius: 10px;
             background: #09C9D9;
         }
+        .table-wrapper{
+            padding:10px 25px 10px;
+            background: rgba( 62, 62, 62, 0.50 );
+            width:90%;
+            box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+            backdrop-filter: blur( 5.0px );
+            -webkit-backdrop-filter: blur( 5.0px );
+            border-radius: 10px;
+            border: 1px solid rgba( 255, 255, 255, 0.18 );
+            table {
+                color: white;
+                width:100%;
+                th {
+                    text-align: left;
+                    border-left: 1px solid #0aa;
+                }
 
-        table {
-            color: black;
-            width: 100%;
+                td {
+                    border-left: 1px solid #0aa;
+                }
 
-            th {
-                text-align: left;
-                border-left: 1px solid #0aa;
-            }
-
-            td {
-                border-left: 1px solid #0aa;
-            }
-
-            .td-button {
-                width: 30px;
-                border-left: none;
+                .td-button {
+                    width: 30px;
+                    border-left: none;
+                }
             }
         }
     }
